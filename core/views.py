a8fb09fff2e67ee3cccff4a3cde12311b07e0a3b
Islam412 <islam.124.hamdy@gmail.com>
2023-10-26 15:48:39 +0300

    verify your login and you login in site

--- core/views.py
+++ core/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render , redirect
 from django.contrib.auth.models import User , auth
 from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 from .models import Profile
 from django.http import HttpResponse
 
 
+@login_required(login_url='signin')
 def index(request):
     return render(request, 'index.html')
 
@@ -62,3 +64,5 @@ def signin(request):
 def logout(request):
     auth.logout(request)
     return redirect('signin')
+
+
