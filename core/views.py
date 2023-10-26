2ee4765da31c107f0b921ffc2f4dfbdb312c2594
Islam412 <islam.124.hamdy@gmail.com>
2023-10-26 03:31:34 +0300

    run the account creation page

--- core/views.py
+++ core/views.py
@@ -1,5 +1,7 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render , redirect
+from django.contrib.auth.models import User , auth
+from django.contrib import messages
+# from django.http import HttpResponse
 
 
 def index(request):
@@ -7,4 +9,25 @@ def index(request):
 
 
 def signup(request):
-    return render(request,'signup.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request,'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request,'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username,email=email,password=password)
+                user.save()
+                messages.success(request,'The account has been created successfully')
+                return redirect('signup')
+        else:
+            messages.info(request,'The Password Not Matching')
+            return redirect('signup')
+    else:
+        return render(request,'signup.html')
