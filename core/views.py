91efb1ef81fa8b554b7f4a955e5bb62d1e9fdfba
Islam412 <islam.124.hamdy@gmail.com>
2023-10-28 05:28:54 +0200

    update views profile user

--- core/views.py
+++ core/views.py
@@ -57,14 +57,14 @@ def like_post(request):
 @login_required(login_url='signin')
 def profile(request,pk):
     user_object = User.objects.get(username=pk)
-    user_profile = Profile.user.get(user=user_object)
-    user_post = Post.objects.filter(user=pk)
-    user_post_length = len(user_post)
+    user_profile = Profile.objects.get(user=user_object)
+    user_posts = Post.objects.filter(user=pk)
+    user_post_length = len(user_posts)
 
     context = {
         'user_object':user_object,
         'user_profile':user_profile,
-        'user_post':user_post,
+        'user_posts':user_posts,
         'user_post_length':user_post_length,
     }
     return render(request,'profile.html',context)
