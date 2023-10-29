24c57a24f285a466eb5ab993f331e1ea194f5c79
Islam412 <islam.124.hamdy@gmail.com>
2023-10-29 22:05:39 +0200

    desing views search p1

--- core/views.py
+++ core/views.py
@@ -49,8 +49,27 @@ def upload(request):
 
 @login_required(login_url='signin')
 def search(request):
+    user_object = User.objects.get(username=request.user.username)
+    user_profile = Profile.objects.get(user=user_object)
 
-    return render(request,'search.html')
+    if request.method == 'POST':
+        username = request.POST['username']
+        username_object = User.objects.filter(username__icontains=username)
+
+        username_profile = []
+        username_profile_list = []
+
+        for user in username_object:
+            username_profile.append(user.id)
+
+        for id in username_profile:
+            profile_lists = Profile.objects.filter(id=id)
+            username_profile_list.append(profile_lists)
+
+        username_profile_list = list(chain(*username_profile_list))
+
+
+    return render(request,'search.html',{'user_profile':user_profile, 'username_profile_list':username_profile_list})
 
 
 
